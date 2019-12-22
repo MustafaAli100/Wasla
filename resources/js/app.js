8fba9c877f1ed8618bec2f33a64eaf3ce92a76cd
awdaf-azhri <fadwa.azhri66@gmail.com>
2019-12-22 06:13:45 -0800

    major changes

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
 require('./bootstrap');
+require('./jquery.min');
+require('./script');
+require('./atlantis.min');
+require('./jquery.scrollbar.min');
+require('./jquery.sparkline.min');
+require('./jquery.ui.min');
+require('./webfont.min');
