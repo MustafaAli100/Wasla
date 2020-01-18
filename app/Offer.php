79f0d146fd8e6a802d0d2dfad51c4ff946978f73
MustafaAli100 <caan1006@gmail.com>
2020-01-19 02:05:31 +0300

    show project complete

--- app/Offer.php
+++ app/Offer.php
@@ -7,6 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Offer extends Model
 {
     //
+    protected $fillable = [
+        'project_id', 'oprice','datetime',
+    ];
     public function project()
     {
         return $this->belongsTo(Project::class);
--- resources/views/owner/show_project.blade.php
+++ resources/views/owner/show_project.blade.php
@@ -21,9 +21,7 @@
 						<div class="d-flex">
 							<div class="flex-1 ml-3 pt-1">
 								<h4 class="text-uppercase fw-bold mb-1"> Project details  </h4>
-                                <p class=""> d is a command-line utility for Unix and Unix-like operating systems, the primary purpose of which is to convert and copy files.[1]
-									                                                On Unix, device drivers for hardware (such as hard disk drives) and special device files (such as /dev/zero and /dev/random) appear in the file system just like normal files; dd can also read and/or write from/to these files, provided that function is implemented in their respective driver. As a result, dd can be used for tasks such as backing up the boot sector of a hard drive, and obtaining a fixed amount of random data. The dd program can also perform conversions on the data as it is copied, including byte order swapping and conversion to and from the ASCII and EBCDIC text encodings.[2]  
-								</p>
+                                <p class=""> {{$singleproject->description }}								</p>
 							</div>
                         </div>
                     </div>
@@ -34,11 +32,8 @@
 							<div class="d-flex">
 								<div class="flex-1 ml-3 pt-1">
 									<h4 class="text-uppercase fw-bold mb-1"> Skills  </h4>
-									<span class="skill" style="margin-left:20px; color:blue">   Laravel    </span>
-									<span class="skill" style="margin-left:20px; color:blue">    PHP  </span>
-									<span class="skill" style="margin-left:20px; color:blue">    javasCript     </span>
-									<span class="skill" style="margin-left:20px; color:blue">  htmll    </span>
-                       			</div>
+									<span class="skill" style="margin-left:20px; color:blue">   {{ $singleproject->skills }}    </span>
+								</div>
                    			 </div>
                 		</div> 
                		</div>
@@ -49,11 +44,11 @@
 									<div><h4 class="text-uppercase fw-bold mb-1"> Project card </h4> </div>
                                     <div class="col-sm-12 skills" >
 										<span class="skills col-sm-6"> budget  : </span>
-										<i class="col-sm-4"> 2000$ - 5000$ </i >
+										<i class="col-sm-4"> {{ $singleproject->price }}</i >
 									</div><br>
 									<div class="col-sm-12 skills" >
 										<span class="skills col-sm-6">implementation: </span>
-										<i class="col-sm-4" > 6 days </i>
+										<i class="col-sm-4" > {{ $singleproject->time }} </i>
 									</div><br>
 									<div class="col-sm-12 skills" >
 										<span class="skills col-sm-6"> Average offers :  </span>
